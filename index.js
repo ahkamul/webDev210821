915a7d4f0cc5808d3fe07116ee1228d36f0beb05
ahkamul <sasraishougat@gmail.com>
2021-08-21 17:31:57 +0600

    Code on JS Array and JS Date written in index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+// Array Methods:
+
+// var fruits = ["Mango", "Banana", "Pineapple", "Guava", "Coconut", "Apple", "Orange"]
+// var fruits2 = ["Papaya", "Palm", "Lemon", "Lichi"]
+
+// //fruits.pop();
+// //fruits.push("Grape")
+
+// console.log(fruits.length);
+// console.log(fruits);
+// console.log(fruits.concat(fruits2))
+
+
+// JS Date Object: 
+
+//var d = new Date()
+// var d = new Date(1993, 11, 08, 11, 45, 49);
+
+// console.log(d)
+
+var d = new Date()
+
+//console.log(d.getTime())
+//console.log(d.getFullYear())
+//console.log(d.getMonth())
+//console.log(d.getDate())
+
+//console.log(d.setFullYear())
+//console.log(d.setFullYear(2021))
+console.log(d.setFullYear(2020))
